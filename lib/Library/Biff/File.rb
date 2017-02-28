49bb9c5987454c2838540824b910bf09e06cc1b7
Schizohatter <soanvig@gmail.com>
2017-02-28 23:08:52 +0100

    Fix Library to work with Tables

--- lib/Library/Biff/File.rb
+++ lib/Library/Biff/File.rb
@@ -1,6 +1,6 @@
 class Library::Biff::File < Block
   def initialize( source, start )
-    super( source, start, TABLES['biff']['header'] )
+    super( source, start, TABLES['biff']['file'] )
 
     @values[:resource].flatten!
   end
--- lib/Library/Biff/Tileset.rb
+++ lib/Library/Biff/Tileset.rb
@@ -1,5 +1,5 @@
 class Library::Biff::Tileset < Block
   def initialize( source, start )
-    super( source, start, TABLES['biff']['header'] )
+    super( source, start, TABLES['biff']['tileset'] )
   end
 end
