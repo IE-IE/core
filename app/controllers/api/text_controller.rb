f8f3e7d753b67a58075036adf8fe31f4c124eac1
Schizohatter <soanvig@gmail.com>
2017-03-16 17:10:42 +0100

    temp

---
+++ app/controllers/api/text_controller.rb
@@ -0,0 +1,43 @@
+class Api::TextController < ApplicationController
+  include ActionController::Live
+
+   def set
+    result = {}
+    if File.exist? params[:location]
+      result[:success] = true
+      Memory.save( :chitin_location, params[:location] )
+    else
+      result[:success] = false
+      result[:error] = "Location doesn't exist"
+    end
+
+    render json: result
+  end
+
+  def load
+    location = Memory.read( :chitin_location )
+    if location
+      if params['progress']
+        begin
+          response.headers['Content-Type'] = 'text/event-stream'
+          sse = SSE.new( response.stream, retry: 300 )
+          sent_progress = 0
+          chitin = Chitin.new( location ) do |progress|
+            if progress != sent_progress
+              sse.write({ progress: progress })
+              sent_progress = progress
+            end
+          end
+          Memory.save( :chitin, chitin )
+        ensure
+          sse.close
+        end
+      else
+        chitin = Chitin.new( location ) {}
+        Memory.save( :chitin, chitin )
+        result = { success: !!chitin }
+        render json: result
+      end
+    end
+  end
+end
