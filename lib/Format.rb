a8de151571bd498b549b8d644650443aaec86823
Schizohatter <soanvig@gmail.com>
2017-04-06 11:28:29 +0200

    [Format] Fix offset

--- lib/Format.rb
+++ lib/Format.rb
@@ -1,11 +1,12 @@
 class Format
   def recreate_multiple( params = {} )
     result = []
+    offset = params[:offset]
 
     print params[:message][:start] if params[:message] && params[:message][:start]
 
     params[:count].times do |i|
-      object = params[:klass].new( params[:bytes], params[:offset] )
+      object = params[:klass].new( params[:bytes], offset )
       offset = object.end
       result << object
 
