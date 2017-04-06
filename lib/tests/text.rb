362fa1a9cf3d409e572268d4c62b14aaa7bbb0d4
Schizohatter <soanvig@gmail.com>
2017-04-06 11:47:00 +0200

    [Tests] Fix Text

--- lib/tests/text.rb
+++ lib/tests/text.rb
@@ -2,13 +2,13 @@ require_relative '../app'
 
 dialog_location = "/home/mortimer/PlayOnLinux's virtual drives/bgee/drive_c/Program Files/Baldur's Gate/Dialog.tlk"
 
-text = Text.new( dialog_location )
+text = Text.new( dialog_location ) {}
 
 2.times do |n|
   puts text.entries[516 + n][:string]
 end
 
-text = Text.new( dialog_location )
+text = Text.new( dialog_location ) {}
 
 2.times do |n|
   puts text.entries[516 + n][:string]
