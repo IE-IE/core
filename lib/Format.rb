6f595a94ebd85cd2007741acbcd57b1d485218fc
Schizohatter <soanvig@gmail.com>
2017-04-06 11:19:29 +0200

    [Format] Add printing messages

--- lib/Format.rb
+++ lib/Format.rb
@@ -2,6 +2,8 @@ class Format
   def recreate_multiple( params = {} )
     result = []
 
+    print params[:message][:start] if params[:message] && params[:message][:start]
+
     params[:count].times do |i|
       object = params[:klass].new( params[:bytes], params[:offset] )
       offset = object.end
@@ -9,6 +11,8 @@ class Format
 
       yield params[:progressbar].tick if params[:progressbar]
     end
+
+    puts params[:message][:end] if params[:message] && params[:message][:end]
 
     result
   end
